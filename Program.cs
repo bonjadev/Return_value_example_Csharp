a99386b8af5fe9a9df196707851397161e26c94e
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-04-29 06:31:43 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace C__Return_Values
+{
+    internal class Program
+    {
+        static int MyMethod(int x)
+        {
+            return 5 + x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMethod(3));
+        }
+    }
+}
